c44cd46e111707c57f77fc9783942354b78164d6
HrHossain <mofazzelhossain185@gmail.com>
2026-09-10 18:00:59 +0600

    Update App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -57,7 +57,7 @@ function App() {
             <Route index element={<AdminDashboard/>}/>
             <Route path="products" element={<AdminProducts/>}/>
             <Route path="products/new" element={<AdminProductForm/>}/>
-            <Route path="products/:id/edit" element={<AdminProducts/>}/>
+            <Route path="products/:id/edit" element={<AdminProductForm/>}/>
             <Route path="orders" element={<AdminOrders/>} />
             <Route path="delivery-partners" element={<AdminDeliveryPartners/>} />
           </Route>
